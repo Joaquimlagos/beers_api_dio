8f071c48a6cf55bfe905aaea8207fb73c7d5edf4
Joaquim Lagos <joaquim.lagos2000@gmail.com>
2021-09-23 01:53:22 -0300

    criação do projeto spring-boot

---
+++ src/main/java/com/digitalinnovation/beer_api/BeerApiApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.beer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalinnovation/beer_api/BeerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.beer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
