dadef9e049aae37f36740c2cb741899fb106a592
Joaquim Lagos <joaquim.lagos2000@gmail.com>
2021-09-30 21:05:54 -0300

    criação e testes de busca de cervejas pelo tipo

--- src/main/java/com/digitalinnovation/beer_api/exception/BeerNotFoundException.java
+++ src/main/java/com/digitalinnovation/beer_api/exception/BeerNotFoundException.java
@@ -1,5 +1,6 @@
 package com.digitalinnovation.beer_api.exception;
 
+import com.digitalinnovation.beer_api.enums.BeerType;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -9,6 +10,10 @@ public class BeerNotFoundException extends Exception {
     public BeerNotFoundException(String beerName) {
         super(String.format("Beer with name %s not found in the system.", beerName));
     }
+
+   /* public BeerNotFoundException(BeerType type) {
+        super(String.format("Beer with type %s not found in the system.", type));
+    } */
 
     public BeerNotFoundException(Long id) {
         super(String.format("Beer with id %s not found in the system.", id));
--- src/main/java/com/digitalinnovation/beer_api/repository/BeerRepository.java
+++ src/main/java/com/digitalinnovation/beer_api/repository/BeerRepository.java
@@ -1,11 +1,14 @@
 package com.digitalinnovation.beer_api.repository;
 
 import com.digitalinnovation.beer_api.entity.Beer;
+import com.digitalinnovation.beer_api.enums.BeerType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BeerRepository extends JpaRepository<Beer, Long> {
 
     Optional<Beer> findByName(String name);
+    List<Beer> findByType(BeerType type);
 }
